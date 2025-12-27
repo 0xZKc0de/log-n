abad4a198c2e80d835205b356d019051fc339527
Mohamed EL HADDAD <elhaddadmohamed963@gmail.com>
2025-12-27 10:58:52 +0100

    Setting up backend

---
+++ log-n-Server/src/main/java/com/haddad/lognserver/LogNServerApplication.java
@@ -0,0 +1,13 @@
+package com.haddad.lognserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogNServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogNServerApplication.class, args);
+    }
+
+}
---
+++ log-n-Server/src/test/java/com/haddad/lognserver/LogNServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haddad.lognserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogNServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
